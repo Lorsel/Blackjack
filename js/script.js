47fec9fce1f253723901168814758ae0fe104194
Zefiro05 <116448603+Zefiro05@users.noreply.github.com>
2022-11-11 16:49:38 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
 const mat = [["C",[1,2,3,4,5,6,7,8,9,10,11,12,13]],["Q",[1,2,3,4,5,6,7,8,9,10,11,12,13]],["F",[1,2,3,4,5,6,7,8,9,10,11,12,13]],["P",[1,2,3,4,5,6,7,8,9,10,11,12,13]]];
 var ico = [[],[],[],[]];
 var doc = document.getElementById("test");
-
 for(var i=0;i<4;i++){
     for(var j=0;j<13;j++){
         ico[i][j] = "ico/" + i + "/" + (j+1) + ".jpg";
@@ -19,3 +18,9 @@ doc.innerHTML += "<img id='f25' src='ico/fish/fish_25.png'> </br>";
 doc.innerHTML += "<img id='f100' src='ico/fish/fish_100.png'> </br>";
 doc.innerHTML += "<img id='f500' src='ico/fish/fish_500.png'> </br>";
 doc.innerHTML += "<img id='logo' src='ico/icona.png'> </br>";
+
+function chiudi(){
+    alert("EH VOLEVI, GUARDA CHE FACCIA NON SE LO ASPETTAVA!!")
+    window.top.close();
+}
+
