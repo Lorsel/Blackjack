267de9c727548606e79a0aea3bf3f068998323e9
Lorsel <lorenzo.stella.1408@gmail.com>
2022-11-12 23:55:20 +0100

    card list

--- js/card_list.js
+++ js/card_list.js
@@ -4,6 +4,6 @@ for(var i=0;i<10;i++){
     for(var j=0;j<4;j++) {
         doc.innerHTML += "<img src='../ico/" + j + "/" + i + ".png'>";
     }
-    doc.innerHTML += "<h3>     -->  Questa carta vale" + i+1 + "</h3>";
-    doc.innerHTML += "<br/>"
+    doc.innerHTML += "<h3>Questa carta vale " + (i+1) + "</h3>";
+    doc.innerHTML += "<br/>";
 }
