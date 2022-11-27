a103ddcff4d9f276c57fe83a5c7fb9424ef0ead0
Lorsel <lorenzo.stella.1408@gmail.com>
2022-11-27 14:30:32 +0100

    parentesi di merda

--- js/script.js
+++ js/script.js
@@ -264,15 +264,15 @@ function double_down(){
     }
 }
 /*il giocatore si ferma, bloccando il punteggio e le puntate, fino a fine game--FINITO*/
-function stand(){
-    if(nowplaying == true){
-    /*il giocatore si ferma, bloccando il punteggio e le puntate, fino a fine game*/
-    if(dataBase[whoPlaying].lost || dataBase[whoPlaying].standed){
-        alert("Non puoi fermarti perchè hai perso o ti sei già fermato");
-    }
-    else {
-        dataBase[whoPlaying].standed = true;
-        nextPlayer();
+function stand() {
+    if (nowplaying == true) {
+        /*il giocatore si ferma, bloccando il punteggio e le puntate, fino a fine game*/
+        if (dataBase[whoPlaying].lost || dataBase[whoPlaying].standed) {
+            alert("Non puoi fermarti perchè hai perso o ti sei già fermato");
+        } else {
+            dataBase[whoPlaying].standed = true;
+            nextPlayer();
+        }
     }
 }
 
